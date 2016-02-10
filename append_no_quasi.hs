1119c09e20f8a2053bf08b3fbd5b7adb521f8687
David Casperson <David.Casperson@unbc.ca>
2016-02-09 16:29:45 -0800

    Added missing example.

---
+++ append_no_quasi.hs
@@ -0,0 +1,16 @@
+[ C (Clause (Struct "append"
+             [(Struct "[]" []),
+              VariableName 0 "X",
+              VariableName 0 "X"]) []),
+  C (Clause
+     (Struct "append"
+      [Struct "." [VariableName 0 "X",
+                   VariableName 0 "Xs"],
+       VariableName 0 "Ys",
+       Struct "." [VariableName 0 "X",
+                   VariableName 0 "Zs"]])
+     [(Struct "append"
+      [VariableName 0 "Xs",
+       VariableName 0 "Ys",
+       VariableName 0 "Zs"])])
+]
