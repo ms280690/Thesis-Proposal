eb35f5dff74e33307b1cc43a00ee4739ca0111df
ms280690 <ms280690@gmail.com>
2016-02-25 16:10:01 -0800

    polished code and function names in proto3.tex section 12.6.2.

--- haskell-proto3-bevy-icebox.hs
+++ haskell-proto3-bevy-icebox.hs
@@ -11,7 +11,7 @@ monadicUnification t1 t2 = do
   x3 <- U.unify x1 x2
   return $! (x3, d1 `Map.union` d2)
 
-goUnify ::
+runUnify ::
   (forall s. (BindingMonad FTS (STVar s FTS) (ST.STBinding s))
   => (ErrorT
         (UT.UFailure FTS (ST.STVar s FTS))
@@ -19,16 +19,16 @@ goUnify ::
         (UT.UTerm FTS (ST.STVar s FTS),
             Map Id (ST.STVar s FTS))))
   -> [(Id, Prolog)]
-goUnify test = ST.runSTBinding $ do
-  answer <- runErrorT $ test --ERROR
+runUnify test = ST.runSTBinding $ do
+  answer <- runErrorT $ test
   case answer of
     (Left _)            -> return []
-    (Right (_, dict))   -> f1 dict
+    (Right (_, dict))   -> extractUnifier dict
 
-f1 :: (BindingMonad FTS (STVar s FTS) (ST.STBinding s))
+extractUnifier :: (BindingMonad FTS (STVar s FTS) (ST.STBinding s))
   => (forall s. Map Id (STVar s FTS)
       -> (ST.STBinding s [(Id, Prolog)]))
-f1 dict = do
+extractUnifier dict = do
   let ld1 = Map.toList dict
   ld2 <- sequence [ v1 | (k,v) <- ld1, let v1 = UT.lookupVar v]
   let ld3 = [ (k,v) | ((k,_),Just v) <- ld1 `zip` ld2]
@@ -39,7 +39,7 @@ substConvertor :: [(Id, Prolog)] -> [Subst]
 substConvertor xs = Prelude.map (\(varId, p) -> (->-) varId 
                               (unFlatten $ unP $ p)) xs 
 
-unify t1 t2 = substConvertor (goUnify (monadicUnification t1 t2))
+unify t1 t2 = substConvertor (runUnify (monadicUnification t1 t2))
 
 varX :: Term
 varX = Var (0,"x")
